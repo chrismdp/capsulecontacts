ed90cb41af847c4c37cce354cb721aa2c27ae44f
Chris Parsons <chris@edendevelopment.co.uk>
2010-10-23 20:41:37 +0100

    Initial commit of capsule contacts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capsulecontacts::Application.config.secret_token = '1aaa35dce5d178c4e924a8d80a0f2f701754bd95dcb0ab10a094b4ca1584247cf42d99ec8f70e653e94b10d714083c4b9a0e7c2fe4db7d24fa045bc1a04ede55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Capsulecontacts::Application.config.session_store :cookie_store, :key => '_capsulecontacts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Capsulecontacts::Application.config.session_store :active_record_store
